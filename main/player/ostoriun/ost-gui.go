1b21015085c347145032df946bdedf14ba9ff61b
Dave Jorgenson <jorgenson.dave@gmail.com>
2021-01-02 15:40:21 -0500

    character selection, skill building for dynamic gui load and message passing

---
+++ main/player/ostoriun/ost-gui.go
@@ -0,0 +1,15 @@
+components {
+  id: "ostoriun"
+  component: "/main/player/ostoriun/ostoriun.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
